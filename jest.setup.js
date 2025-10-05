5c256c0688d12e897699f790d18224b608ce1242
mangobanaani <psilt@iki.fi>
2025-10-05 13:16:27 +0300

    fix mock webGL test issue

--- jest.setup.js
+++ jest.setup.js
@@ -3,14 +3,14 @@ import '@testing-library/jest-dom'
 // Mock WebGL context for testing
 class MockWebGL2RenderingContext {
   canvas = { width: 800, height: 600 }
-  
+
   createShader() { return {} }
   createProgram() { return {} }
   createBuffer() { return {} }
   createTexture() { return {} }
   createFramebuffer() { return {} }
   createVertexArray() { return {} }
-  
+
   shaderSource() {}
   compileShader() {}
   getShaderParameter() { return true }
@@ -20,42 +20,42 @@ class MockWebGL2RenderingContext {
   getProgramParameter() { return true }
   getProgramInfoLog() { return '' }
   useProgram() {}
-  
+
   getUniformLocation() { return {} }
   uniform1f() {}
   uniform1i() {}
   uniform2f() {}
-  
+
   bindBuffer() {}
   bufferData() {}
   vertexAttribPointer() {}
   enableVertexAttribArray() {}
-  
+
   bindVertexArray() {}
   bindTexture() {}
   texImage2D() {}
   texParameteri() {}
-  
+
   bindFramebuffer() {}
   framebufferTexture2D() {}
   checkFramebufferStatus() { return 36053 } // FRAMEBUFFER_COMPLETE
-  
+
   viewport() {}
   clear() {}
   drawArrays() {}
   blitFramebuffer() {}
-  
+
   deleteShader() {}
   deleteProgram() {}
   deleteBuffer() {}
   deleteTexture() {}
   deleteFramebuffer() {}
   deleteVertexArray() {}
-  
+
   getParameter() { return 'Mock WebGL Renderer' }
   getExtension() { return null }
   getShaderPrecisionFormat() { return { precision: 23 } }
-  
+
   // Constants
   VERTEX_SHADER = 35633
   FRAGMENT_SHADER = 35632
@@ -86,6 +86,9 @@ class MockWebGL2RenderingContext {
   HIGH_FLOAT = 36338
   MEDIUM_FLOAT = 36337
 }
+
+// Make MockWebGL2RenderingContext available globally for tests
+global.MockWebGL2RenderingContext = MockWebGL2RenderingContext
 
 // Mock HTMLCanvasElement.getContext
 const originalGetContext = HTMLCanvasElement.prototype.getContext
